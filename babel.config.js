e576abd5dc4eb37d976c3f3e6305cc0f1e86a0b4
Lucas Silveira <lugano1995@gmail.com>
2020-07-02 19:21:44 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@configs': './src/configs',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
+};
---
+++ src/tests/TextExample.spec.ts
@@ -0,0 +1,3 @@
+test('it should be ok', () => {
+  expect(true).toBe(true);
+});
